3083f117e7ee386ad853f519e53e44bf4de886ff
chayadeaver <chayadeaver@gmail.com>
2020-03-02 10:44:12 -0500

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :menus
-  resources :items
-  resources :orders
-  resources :carts
+  # resources :categories
+  # resources :menus
+  # resources :items
+  # resources :orders
+  # resources :carts
+  root to: 'home#index'
   devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
