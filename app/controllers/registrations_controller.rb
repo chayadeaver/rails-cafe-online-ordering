21bc1181a2deddfe791b5aa4b1c038e92b2ffaa2
chayadeaver <chayadeaver@gmail.com>
2020-03-03 15:29:55 -0500

    added strong params and new routes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
     private
         def sign_up_params
-            params.require(:users).permit(:name, :email, :password, :password_confirmation, :admin)
+            params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
         end
 
         def account_update_params
-            
+            params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
         end
 end
