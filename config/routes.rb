a019706330490fdf0e13b951ccca6bbe98308163
chayadeaver <chayadeaver@gmail.com>
2020-03-03 14:27:15 -0500

    changed root page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # resources :items
   # resources :orders
   # resources :carts
-  root to: 'home#index'
+  root to: 'application#welcome'
   devise_for :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
