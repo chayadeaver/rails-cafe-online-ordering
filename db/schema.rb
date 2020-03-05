2b02e00588f02bab0648e3b048003faffd0e1210
chayadeaver <chayadeaver@gmail.com>
2020-03-04 20:45:44 -0500

    add order_id to items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_214822) do
+ActiveRecord::Schema.define(version: 2020_03_04_202632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_214822) do
     t.integer "menu_id"
     t.integer "category_id"
     t.integer "cart_id"
+    t.integer "order_id"
   end
 
   create_table "menus", force: :cascade do |t|
