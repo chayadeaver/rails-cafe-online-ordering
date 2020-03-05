d5c00546704302f4efc4346c0f6b0fc32013f2a6
chayadeaver <chayadeaver@gmail.com>
2020-03-05 15:24:20 -0500

    added optional

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
-    belongs_to :category
-    belongs_to :menu
-    belongs_to :order
-    belongs_to :cart
+    belongs_to :category, optional: true
+    belongs_to :menu, optional: true
+    belongs_to :order, optional: true
+    belongs_to :cart, optional: true
     validates :name, presence: true
-    validates :name, uniqueness: true
 
+    # validates :menu_id, presence: true, blank: false, nil: false
+    # validates :category_id, presence: true, blank: false, nil: false
 end
