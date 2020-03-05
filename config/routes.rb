ae4d5c6a554e1cf38a8f0cb38924335b32e417f4
chayadeaver <chayadeaver@gmail.com>
2020-03-05 15:24:46 -0500

    added menu index page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     get '/login', to: 'devise/sessions#new'
     get '/signup', to: 'devise/registrations#new'
   end
+  get '/menu', to: 'menus#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
