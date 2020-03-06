48bb1b5a4bc39182d7e474f6c1e791d50e89f3af
chayadeaver <chayadeaver@gmail.com>
2020-03-06 15:41:40 -0500

    Uncommented resouces in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  # resources :categories
-  # resources :menus
-  # resources :items
-  # resources :orders
-  # resources :carts
+  resources :categories
+  resources :menus
+  resources :items
+  resources :orders
+  resources :carts
   root to: 'application#welcome'
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
   devise_scope :user do
     get '/login', to: 'devise/sessions#new'
     get '/signup', to: 'devise/registrations#new'
   end
-  get '/menu', to: 'menus#index'
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
