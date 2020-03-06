0f76f70cf0fab0ae3b4e299bc9fbbb1291022cde
chayadeaver <chayadeaver@gmail.com>
2020-03-06 16:47:45 -0500

    admin_only method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
     def welcome
     end
 
+    def admin_only
+        unless current_user.admin
+            flash[:notice] = "You must be an admin to perform that function!"
+            redirect_to user_session_path(current_user)
+        end
+    end
 end
