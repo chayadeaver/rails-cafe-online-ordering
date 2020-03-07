ae517a4205c8d643c5f9a8168232ebbfcf6b414c
chayadeaver <chayadeaver@gmail.com>
2020-03-07 11:59:47 -0500

    CRUD actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,49 @@
 class CategoriesController < ApplicationController
+    before_action :find_category, only: [:show, :edit, :update]
+    before_action :admin_only
     def index
         @categories = Category.all
     end
 
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.create(category_params)
+        if @category
+            redirect_to category_path(@category)
+        else
+            render :new
+        end
+    end
+
     def show
+    end
+
+    def edit
+    end
+
+    def update
+        @category.update(category_params)
+        if @category.save
+            redirect_to category_path(@category)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        Category.find_by(id: params[:id]).destroy
+        redirect_to categories_path
+    end
+
+private
+    def find_category
         @category = Category.find_by(id: params[:id])
+    end
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,8 +1,49 @@
 class MenusController < ApplicationController
+    before_action :find_menu, only: [:show, :edit, :update]
+    before_action :admin_only
     def index
         @menus = Menu.all
     end
+
+    def new
+        @menu = Menu.new
+    end
+
+    def create
+        @menu = Menu.create(menu_params)
+        if @menu
+            redirect_to menu_path(@menu)
+        else
+            render :new
+        end
+    end
+
     def show
+    end
+
+    def edit
+    end
+
+    def update
+        @menu.update(menu_params)
+        if @menu.save
+            redirect_to menu_path(@menu)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        Menu.find_by(id: params[:id]).destroy
+        redirect_to menus_path
+    end
+
+private
+    def find_menu
         @menu = Menu.find_by(id: params[:id])
+    end
+
+    def menu_params
+        params.require(:menu).permit(:name)
     end
 end
