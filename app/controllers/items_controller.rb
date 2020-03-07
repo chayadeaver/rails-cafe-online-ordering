329639e864a490118b37a102d5571c0fc3a8b900
chayadeaver <chayadeaver@gmail.com>
2020-03-07 12:00:04 -0500

    Menu view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+    def index
+        @menus = Menu.all 
+        @categories = Category.all
+        
+
+        @items = Item.where(nil)
+        @items = @items.by_menu(params[:menu]) if params[:menu].present?
+        @items = @items.by_category(params[:category]) if params[:category].present?
+    end
 end
