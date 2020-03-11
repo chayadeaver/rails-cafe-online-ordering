20330c1b7342c7c2dda9a47402c9f9d91e2223ac
chayadeaver <chayadeaver@gmail.com>
2020-03-11 16:20:45 -0400

    changed notices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
 
     def admin_only
         unless current_user.admin
-            flash[:notice] = "You must be an admin to perform that function!"
-            redirect_to users_show_path(current_user)
+            redirect_to users_show_path(current_user), notice: "You must be an admin to perform that function!"
         end
     end
 
